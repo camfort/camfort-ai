d29b014f71b0d37ccd58e5e2a5f3dc1e269937da
Matthew Danish <mrd45@cam.ac.uk>
2022-11-10 15:58:08 +0000

    gather remaining command line arguments to form search query text

--- search_vectors.py
+++ search_vectors.py
@@ -13,18 +13,20 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--database', '-D', type=str, default=defaultdbfile, help='SQLite3 database filename for storing vectors')
     parser.add_argument('--api-key', type=str, default=None, help='OpenAI API key (or use env var OPENAI_API_KEY)')
-    parser.add_argument('input', type=str, help='Search terms')
+    parser.add_argument('input', nargs=argparse.REMAINDER, type=str, help='Search terms')
 
     args = parser.parse_args()
     if args.api_key is not None:
         api_key = args.api_key
+
+    searchtxt = ' '.join(args.input)
 
     con = sqlite3.connect(args.database)
 
     query = "SELECT path, name AS fname, firstLine, lastLine, vectorid, elem AS emb FROM embeddings JOIN vectors ON embeddings.vectorid = vectors.id ORDER BY vectorid, ord"
     df = pd.read_sql_query(query, con).groupby('vectorid').agg({'emb': list, 'path': 'first', 'fname': 'first', 'firstLine': 'first', 'lastLine': 'first'}).reset_index()
 
-    search_functions(df, args.input)
+    search_functions(df, searchtxt)
 
 def search_functions(df, code_query, n=3, pprint=True, n_lines=7):
     embedding = get_embedding(code_query, engine='code-search-babbage-text-001')
